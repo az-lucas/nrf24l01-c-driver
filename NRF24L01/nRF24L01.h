9f99cbf45c7dc86ace3db0e70b803988da0e2c4d
Lucas Azeredo <lucascmpa@yahoo.com.br>
2025-09-05 23:26:45 -0300

    Adicionado nRF24L01.c e nRF24L01.h. / Add nRF24L01.c and nRF24L01.h.

---
+++ NRF24L01/nRF24L01.h
@@ -0,0 +1,13 @@
+/**
+ * @file    nRF24L01.h
+ * @brief   Biblioteca genérica em C para o transceptor nRF24L01.
+ *
+ * @author  Lucas T. Azeredo
+ * @date    2025-09-05
+ *
+ * @license MIT
+ *
+ * @note    
+ *          
+ *          
+ */
